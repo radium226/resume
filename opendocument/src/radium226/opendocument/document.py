4d2c63f62bfd5b7ed2b216e7fec142730f35d204
radium226 <adrien.besnard@gmail.com>
2023-07-17 08:15:46 +0200

    First step to add Documents

--- opendocument/src/radium226/opendocument/document.py
+++ opendocument/src/radium226/opendocument/document.py
@@ -16,19 +16,59 @@ from .xml import create_element
 @dataclass
 class Document():
 
+    """
+    drwxr-xr-x  2 adrien adrien      3 Jun 26 17:25 META-INF
+    -rw-r--r--  1 adrien adrien 460405 Jun 26 17:25 CV.odt
+    drwxr-xr-x  2 adrien adrien      3 Jun 26 17:25 Thumbnails
+    -rw-r--r--  1 adrien adrien  46976 Jun 26 17:25 styles.xml
+    -rw-r--r--  1 adrien adrien  14404 Jun 26 17:25 settings.xml
+    drwxr-xr-x  2 adrien adrien     18 Jun 26 17:25 Pictures
+    -rw-r--r--  1 adrien adrien     39 Jun 26 17:25 mimetype
+    -rw-r--r--  1 adrien adrien   1051 Jun 26 17:25 meta.xml
+    -rw-r--r--  1 adrien adrien    899 Jun 26 17:25 manifest.rdf
+    -rw-r--r--  1 adrien adrien 174395 Jun 26 17:25 content.xml
+    """
+
     type: DocumentType
+
+    styles: _Element | None = field(init=False)
+
+    settings: _Element | None = field(init=False)
+
+    meta: _Element | None = field(init=False)
+
+    file_path: InitVar[Path] = field(default_factory=lambda: Path(__file__).parent / "data" / "empty.fodt")
 
     content: Content = field(default_factory=Content)
 
+    def __post_init__(self, file_path: Path):
+        match self._storage_type_from_file(file_path):
+            case StorageType.XML_FLAT:
+                self._read_xml_flat(file_path)
+
+            case StorageType.ZIP_CONTAINER:
+                self._read_zip_container(file_path)
+
+    def _storage_type_from_file(file_path: Path) -> StorageType:
+        file_extension = file_path.suffix.lower()
+        storage_types_by_file_extension = {
+            '.odt': StorageType.ZIP_CONTAINER,
+            '.fodt': StorageType.XML_FLAT,
+        }
+        return storage_types_by_file_extension[file_extension]
+
+    def _read_xml_flat(self, file_path) -> None:
+        with file_path.open("r") as file_stream:
+            element_tree = parse(input_stream)
+            document_element = element_tree.getroot()
+
+        
     def __iadd__(self, block: Block) -> None:
         self.content += block
         return self
 
     def save_as(self, file_path: Path) -> None:
-        storage_type = {
-            '.odt': StorageType.ZIP_CONTAINER,
-            '.fodt': StorageType.XML_FLAT,
-        }[file_path.suffix.lower()]
+        
 
         with file_path.open("wb") as output_stream:
             output_stream.write(self.serialize(storage_type))
@@ -42,7 +82,7 @@ class Document():
                 return self._write_zip_container()
 
     def _write_xml_flat(self) -> bytes:
-        empty_file_path = Path(__file__).parent / "data" / "empty.fodt"
+        empty_file_path = 
         with empty_file_path.open("r") as stream:
             element_tree = parse(stream)
             document_element = element_tree.getroot()
@@ -70,5 +110,3 @@ class Document():
 
     def _write_zip_container(self) -> bytes:
         raise Exception("Not yet implemented! ")
-
-        
